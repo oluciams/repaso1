4c0140bb1ef4c806ec71318dce0d11010b3e3975
olga <oluciams84@gmail.com>
2021-04-19 11:28:17 -0500

    adicionar otra prueba mas

---
+++ app.js
@@ -0,0 +1,21 @@
+
+function hello (name){
+    return "Hola " + name.toUpperCase()
+}
+console.log (hello("Pedro"))
+
+/*
+let obj= {
+   nombre: "Maria",
+   edad: 20 
+}
+console.log("Hola " + obj.nombre + " tienes " + obj.edad + " años")
+*/
+/*
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let suma = 0
+for (let i=0; i<arr.length; i++){
+    suma+= arr[i]
+}
+console.log (suma)
+*/
